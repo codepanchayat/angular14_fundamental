66bf63feda99a914689533ab9c26a9b35c7e3f81
unknown <codepanchayat@gmail.com>
2022-08-21 17:54:23 +0530

    Lacture 30

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -22,19 +22,28 @@ export class PassengerDashboardComponent implements OnInit{
     }
 
     handleRemove(event: Passenger){
-      this.passengers = this.passengers.filter((passenger: Passenger)=>{
-        return passenger.id !== event.id
+      this.passengerService
+      .removePassenger(event)
+      .subscribe((data: Passenger)=>{
+        this.passengers = this.passengers.filter((passenger: Passenger)=>{
+          return passenger.id !== event.id
+        })
       })
+      
     }
 
     handleEdit(event: Passenger){
-      
+      this.passengerService
+      .updatePassenger(event)
+      .subscribe((data:Passenger)=>{
         this.passengers = this.passengers.map((passenger: Passenger)=>{
           if(passenger.id === event.id){
             passenger = Object.assign({}, passenger, event)
           }
           return passenger;
-        });    
+        });
+      })
+            
     }
 
 }
--- src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Passenger } from "./models/passenger.interface";
 import { Injectable } from "@angular/core";
-import { HttpClient} from "@angular/common/http";
+import { HttpClient, HttpHeaders} from "@angular/common/http";
 import { Observable } from "rxjs/internal/Observable";
 
 const PASSENGER_API: string = "http://localhost:3000/passengers"
@@ -14,5 +14,15 @@ export class PassengerDashboardService{
     getPassengers(): Observable<Passenger[]>{
       return this.http
       .get<Passenger[]>(PASSENGER_API);
+    }
+
+    updatePassenger(passenger: Passenger): Observable<Passenger>{
+      return this.http
+      .put<Passenger>(`${PASSENGER_API}/${passenger.id}`, passenger);
+    }
+
+    removePassenger(passenger: Passenger): Observable<Passenger>{
+      return this.http
+      .delete<Passenger>(`${PASSENGER_API}/${passenger.id}`);
     }
 }
